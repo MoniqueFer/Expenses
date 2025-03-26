854b9be061b1984dcc56ac98111165ab99367a07
MoniqueFer <138914572+MoniqueFer@users.noreply.github.com>
2025-03-26 10:19:44 -0300

    Initial Commit

---
+++ Expenses/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expenses
+//
+//  Created by Monique Ferrarini on 26/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Monique Ferrarini on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
